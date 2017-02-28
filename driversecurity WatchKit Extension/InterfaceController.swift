6164a30933dd371dc5ed92c07edfd9b1d8f7c619
Cantone Antonio <cantoneandreaantonio@par-41.local>
2017-02-28 11:36:49 +0100

    Initial Commit

---
+++ driversecurity WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  driversecurity WatchKit Extension
+//
+//  Created by Cantone Andrea Antonio on 28/02/17.
+//  Copyright © 2017 Cantone Andrea Antonio. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
